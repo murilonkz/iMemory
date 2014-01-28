cd4934db4e4d0c899be6e0ca492de5638751d3c2
Murilo Campaner <murilo.campaner@AndreFolda.local>
2014-01-28 17:04:12 -0200

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Murilo Campaner on 28/01/14.
+//  Copyright (c) 2014 Murilo Campaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MemoryGameTests/MemoryGameTests.m
@@ -0,0 +1,34 @@
+//
+//  MemoryGameTests.m
+//  MemoryGameTests
+//
+//  Created by Murilo Campaner on 28/01/14.
+//  Copyright (c) 2014 Murilo Campaner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MemoryGameTests : XCTestCase
+
+@end
+
+@implementation MemoryGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
